96ca794bd6ee2cc8f39eb6190235950dec2dfb68
Goma Dangi <darshandristi1@gmail.com>
2022-12-25 13:16:59 +1100

    Three files added

---
+++ main.js
@@ -0,0 +1,13 @@
+/*let links = document.getElementByTagNanme("li");
+let circle = document.getElementById("circle");
+
+for(link of links){
+    link.onclick = function(){
+        for (link of links){
+            link.style.opacity = "1";
+        }
+        circle.style.left = (80 * this.value) + "px";
+        circle.innerHTML = innerHTML;
+        this.style.opacity = "0"
+    }
+}*/
